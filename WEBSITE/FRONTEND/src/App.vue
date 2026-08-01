c2bbc5a1a83f1c69a917913194ea16d00c77dccd
Pintea Dániel <pintearolanddaniel@gmail.com>
2026-08-01 18:00:50 +0200

    Delete WEBSITE directory

--- WEBSITE/FRONTEND/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <RouterView></RouterView>
-</template>
-
-<script setup lang="ts">
-import { useAuthStore } from '@/stores/auth'
-const authStore = useAuthStore()
-authStore.fetchUser()
-</script>
